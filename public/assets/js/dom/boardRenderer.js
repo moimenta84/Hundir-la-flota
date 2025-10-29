6a4c17ccad09c4ee27e5996d508b2d5a4f8d782b
Iker Martínez Velasco <ikerastra@hotmail.com>
2025-10-29 16:36:18 +0100

    feat: implementación del sistema de renderizado del tablero y carga inicial de la interfaz

---
+++ public/assets/js/dom/boardRenderer.js
@@ -0,0 +1,49 @@
+'use strict';
+
+class Renderer {
+
+    /* Recibe el tablero y el ID del 
+       Contenedor donde se pintará el HTML
+    */
+    constructor(board, containerId) {
+        this.board = board;
+        //AQUI SE CAMBIARA POR EL HELPERS//
+        this.container = document.getElementById(containerId);
+    }
+
+    // Renderiza el tablero completo en el DOM
+    render() {
+        // Limpia el contenido anterior del contenedor
+        this.container.innerHTML = '';
+
+        //SE CAMBIARA PORUN HELPERS//
+        const table = document.createElement('table');
+        table.classList.add('board'); // permite estilizar con CSS
+
+        // Recorre filas
+        for (let r = 0; r < this.board.rows; r++) {
+            const tr = document.createElement('tr'); // fila HTML
+
+            // Recorre columnas
+            for (let c = 0; c < this.board.columns; c++) {
+                const td = document.createElement('td'); // celda HTML
+
+                const cell = this.board.getCell(r, c); // obtiene celda del Board
+                td.textContent = cell; // pinta el contenido (*, S, X, O)
+
+                // Añade evento de clic → realizar disparo
+                td.addEventListener('click', () => {
+                    game.shoot(r, c);   // dispara
+                    this.render();     // se vuelve a pintar el tablero
+                });
+
+                tr.appendChild(td); // añade la celda a la fila
+            }
+
+            table.appendChild(tr); // añade la fila a la tabla
+        }
+
+        // Inserta la tabla completa en el contenedor del HTML
+        this.container.appendChild(table);
+    }
+}
